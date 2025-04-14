006607f0ca4caf8d85f43c2a879014fb9bcc7a40
AlfredoF92 <alfred.fiorillo@gmail.com>
2025-04-14 17:55:26 +0200

    field: output con solo due decimali

--- javascript.js
+++ javascript.js
@@ -11,33 +11,32 @@ Bonus:
 Applicare de controlli sull'input dell'utente*/
 
 
-let km = 15; 
+let km = 66; 
 console.log("Km inseriti: " + km);
 
-let eta = 16;
+let eta = 66;
 console.log("Anni inseriti: " + eta);
 
 let prezzoDelBiglietto = km * 0.21; 
 
-let messageOutput;
+console.log("\n***********************************************************************");
 
-let sconto40 = prezzoDelBiglietto * 0.40;
-let prezzoBigliettoScontato40 = prezzoDelBiglietto - sconto40;
-
-console.log("***********************************************************************");
+console.log(">>>>>> Il Prezzo del biglietto è " + prezzoDelBiglietto.toFixed(2) + "€");
 
 if(eta < 18){
     //Applicare sconto del 20% per i minorenni
     let sconto20 = prezzoDelBiglietto * 0.20;
     prezzoDelBiglietto = prezzoDelBiglietto - sconto20;
     console.log("Hai meno di 18 anni, è stato applicato uno sconto del 20% al tuo biglietto!");
+    console.log(">>>>>> Il prezzo del biglietto scontato è: " + prezzoDelBiglietto.toFixed(2) + "€");
 }else if (eta > 65){
     //Applicare sconto del 40% per gli over65
     let sconto40 = prezzoDelBiglietto * 0.40;
-    console.log("Fai parte della categoria over65! E' stato applicato uno sconto del 40% al tuo biglietto!");
+    console.log("Fai parte della categoria Over 65! E' stato applicato uno sconto del 40% al tuo biglietto!");
     prezzoDelBiglietto = prezzoDelBiglietto - sconto40;
+    console.log(">>>>>> Il prezzo del biglietto scontato per te è: " + prezzoDelBiglietto.toFixed(2) + "€");
 }
 
-console.log(">>>>>> Il Prezzo del biglietto è " + prezzoDelBiglietto + "€");
+
 
 console.log("***********************************************************************");
